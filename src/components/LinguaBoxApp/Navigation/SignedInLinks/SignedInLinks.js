fd546c39ee714a06d2947472172a1a7f8f33bf3a
yuliia-havryliuk <gavriliukyulichka@gmail.com>
2021-06-11 14:00:49 +0300

    Added flags folder and added flag to language field

--- src/components/LinguaBoxApp/Navigation/SignedInLinks/SignedInLinks.js
+++ src/components/LinguaBoxApp/Navigation/SignedInLinks/SignedInLinks.js
@@ -1,11 +1,12 @@
 import { RiUser3Fill, RiSettings5Fill } from 'react-icons/ri';
+import flag from './../../../../assets/images/flags/GB.svg.png';
 import s from './SignedInLinks.module.css';
 const SignedInLinks = () => {
   return (
     <div className={s.container}>
       <div className={s.language}>
+        <img className={s.flag} src={flag} alt="flag" />
         <p className={s.languageName}>En</p>
-        {/*<img src={} alt="flag"/>*/}
       </div>
       <RiSettings5Fill className={s.svgImg} />
       <RiUser3Fill className={s.svgImg} />
